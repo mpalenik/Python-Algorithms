f861b90814f952d774b74bba36626538706ad2c5
Mark Palenik <Mark Palenik>
2017-06-02 12:40:59 -0400

    first finished project: neuralnetwork

---
+++ neural network/Main.py
@@ -0,0 +1,91 @@
+import NetworkClasses as nc
+import pylab as pl #needed to plot the output
+
+#simple demonstration of neural network
+#use it to approximate y = 1/x and then retrain
+#to approximate y = x
+
+NeuralNet=nc.Network()
+NeuralNet.alpha = 0.1 #adjust momentum.  Default is 0.1, but this is here to make it easier to adjust on the fly
+NeuralNet.eta = 15.0  #adjust gradient scale factor default is 10.0
+
+#This structure is not necessarily optimized for the problem
+#I picked the number/size of the layers more or less randomly
+NeuralNet.AddLayers([1,8,10,1])
+
+#Some example outputs
+print('Before training')
+print (0.1,NeuralNet.RunNetwork([1]))
+print (0.4,NeuralNet.RunNetwork([4]))
+print (0.6,NeuralNet.RunNetwork([6]))
+
+for i in range(3000):
+    NeuralNet.BackProp([1],[1])
+    NeuralNet.BackProp([2],[0.5])
+    NeuralNet.BackProp([8],[1.0/8.0])
+    NeuralNet.BackProp([4.7],[1.0/4.7])
+    NeuralNet.BackProp([3],[1.0/3.0])
+    NeuralNet.BackProp([7],[1.0/7.0])
+    NeuralNet.BackProp([6.4],[1.0/6.4])
+    NeuralNet.BackProp([1.3],[1.0/1.3])
+
+#The same example outputs
+print('After training')
+print (1,NeuralNet.RunNetwork([1]))
+print (4,NeuralNet.RunNetwork([4]))
+print (6,NeuralNet.RunNetwork([6]))
+
+#Make a plot of y=x and the neural network output vs. x
+xlist = [(1.0+0.1*x) for x in range(0,90)]
+ylist = []
+for x in xlist: ylist.append(NeuralNet.RunNetwork([x]))
+
+pl.plot(xlist,ylist)
+pl.plot(xlist,[1.0/x for x in xlist])
+pl.title('Network trained to produce y = 1/x.  Close to continue')
+pl.show()
+
+#use it to approximate a line y=x
+
+#Clear network so weights are reset to random values
+NeuralNet=nc.Network()
+NeuralNet.alpha = 0.1 #adjust momentum.  Default is 0.1, but this is here to make it easier to adjust on the fly
+NeuralNet.eta = 15.0  #adjust gradient scale factor default is 10.0
+
+#Use the same structure as before, for no particular reason.
+#It shows that the network is at least somewhat robust
+NeuralNet.AddLayers([1,8,10,1])
+
+#Some example outputs
+print('Before training')
+print (0.1,NeuralNet.RunNetwork([0.1]))
+print (0.4,NeuralNet.RunNetwork([0.4]))
+print (0.6,NeuralNet.RunNetwork([0.6]))
+
+for i in range(3000):
+    NeuralNet.BackProp([0],[0])
+    NeuralNet.BackProp([0.1],[0.1])
+    NeuralNet.BackProp([1],[1])
+    NeuralNet.BackProp([0.2],[0.2])
+    NeuralNet.BackProp([0.7],[0.7])
+    NeuralNet.BackProp([0.3],[0.3])
+    NeuralNet.BackProp([0.9],[0.9])
+    NeuralNet.BackProp([0.6],[0.6])
+    NeuralNet.BackProp([0.8],[0.8])
+
+#The same example outputs
+print('After training')
+print (0.1,NeuralNet.RunNetwork([0.1]))
+print (0.4,NeuralNet.RunNetwork([0.4]))
+print (0.6,NeuralNet.RunNetwork([0.6]))
+
+#Make a plot of y=x and the neural network output vs. x
+xlist = [0.1*x for x in range(0,10)]
+ylist = []
+for x in xlist: ylist.append(NeuralNet.RunNetwork([x]))
+
+pl.plot(xlist,ylist)
+pl.plot(xlist,xlist)
+pl.title('Network trained to produce y = x.')
+pl.show()
+
---
+++ neural network/NetworkClasses.py
@@ -0,0 +1,113 @@
+#Simple neural network with backpropagation learning
+#Created by Mark Palenik
+#I created this mainly as an exercise in learning python
+#it's not necessarily the most efficient python code, as it is
+#my first project, but it works.
+
+import random
+import math
+
+class Node:
+    def __init__(self):
+        self.weights = [] #weights connecting to next layer
+        self.invalue = 0  #input from previous layer
+        self.outvalue = 0 #output to next layer
+        self.oldderivs = [] #for momentum
+
+class Layer:
+    def __init__(self):
+        self.Nodes = []
+        self.nnodes = 0
+
+class Network:
+    def __init__(self):
+        self.alpha = 0.1 #momentum
+        self.eta = 10.0   #scale factor on gradient
+        self.Nlayers = 0
+        self.Layers = []
+        
+    def ActivationFunction(self,x):
+        #activation function, bound between 0 and 1
+        return 1.0/(1.0+math.exp(-x))
+    
+    def ActivationDeriv(self,x):
+        #derivative of activation function
+        denom = (1.0+math.exp(-x))
+        return math.exp(-x)/(denom*denom)
+        
+    def AddLayers(self,nNodes):
+        #Add layers to neural network.  nNodes is a list
+        #containing the number of nodes in each layer
+        
+        nAdd = len(nNodes)
+        
+        for i in range(nAdd):
+            L = Layer()
+            L.nnodes=nNodes[i]
+            for j in range(nNodes[i]): L.Nodes.append(Node())
+            self.Layers.append(L)
+            
+            if (self.Nlayers>0):
+                for node in self.Layers[self.Nlayers-1].Nodes:
+                    for j in range(nNodes[i]): node.weights.append(2.0*(random.random()-1.0))
+                    node.oldderivs = [0]*nNodes[i]
+            
+            self.Nlayers = self.Nlayers+1
+            
+    def AddLayer(self,nNodes):
+        #wrapper for AddLayers to add a single layer with nNodes nodes
+        AddLayers(1,[nNodes])
+            
+    def RunNetwork(self,input):
+        #forward propagate the network.  input is a list containing the
+        #input.  Returns a list with the output from each node in the output layer
+        
+        currLayer = self.Layers[0]
+        for i in range(currLayer.nnodes): currLayer.Nodes[i].outvalue = input[i]
+        LastLayer = currLayer
+        
+        for i in range(1,self.Nlayers):
+            currLayer = self.Layers[i]
+            for j in range(currLayer.nnodes):
+                node = currLayer.Nodes[j]
+                node.invalue = 0
+                for k in range(LastLayer.nnodes):
+                    node.invalue = node.invalue + LastLayer.Nodes[k].outvalue*LastLayer.Nodes[k].weights[j]
+                    
+                node.outvalue = self.ActivationFunction(node.invalue)
+                
+            LastLayer = currLayer
+                
+        return [node.outvalue for node in currLayer.Nodes]
+    
+    def BackProp(self,input,desiredOut):
+        #Run one iteration of backwards propagation
+        
+        #Although typically described as a gradient descent, another interpretation of this
+        #agorithm is as the pullback of a one-form of the difference between the desired and
+        #actual output from the space of outputs to the space of weights.
+        
+        derivo = [] #derivative of error function with respect to the output of a given layer
+        #start with the derivative of the error function with respect to the output layer
+        netout = self.RunNetwork(input) #run the error network
+        for i in range(len(input)): derivo.append(desiredOut[i] - netout[i]) #take difference between desired and actual output
+        
+        for i in range(self.Nlayers-2,-1,-1):
+            currLayer = self.Layers[i]
+            derivCurr = [0]*currLayer.nnodes #to build derivative of output with respect to current layer
+            
+            for k in range(self.Layers[i+1].nnodes):
+                node = self.Layers[i+1].Nodes[k]
+                dodx = self.ActivationDeriv(node.invalue) #derivative of node output with respect to input
+                for j in range(currLayer.nnodes):
+                    node2 = currLayer.Nodes[j]
+                    #derivative of error with respect to node j
+                    derivCurr[j] = derivCurr[j]+derivo[k]*dodx*node2.weights[k]
+                    
+                    dEdwj = derivo[k]*dodx*node2.outvalue #derivative of error with respect to weight
+                    node2.weights[k] = node2.weights[k] + (1.0-self.alpha)*self.eta*dEdwj + self.alpha*node2.oldderivs[k] #adjust weight
+                    node2.oldderivs[k] = dEdwj #set momentum derivative
+                    
+            derivo = derivCurr
+                    
+ 
